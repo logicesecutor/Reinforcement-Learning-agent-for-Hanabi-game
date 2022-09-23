1c2c4bfcc57b0c277b16016d4638d5aacf9b9597
logicesecutor <58169924+logicesecutor@users.noreply.github.com>
2022-09-24 01:24:42 +0200

    .

--- hanabi/Game_model.py
+++ hanabi/Game_model.py
@@ -1,22 +1,55 @@
+import random
+import sys
+from typing import List
+
+
+
+CARDS_COLORS = ["red", "yellow", "white", "green", "blue"]
+CARDS_VALUES = {1:3, 2:2, 3:2, 4:2, 5:1}
+
+class Card:
+    def __init__(self, color, value) -> None:
+        self.color = color
+        self.value = value
+
 class Player:
 
-    def __init__(self, name,  hand) -> None:
+    def __init__(self, name, hand: List[Card]) -> None:
         self.name = name
         self.hand = hand
 
-
 class GameManager:
 
-    def __init__(self, player: list) -> None:
-        self.n_player = len(player)
-        self.players = [Player(p.name, p.hand) for p in player]
-        self.
+    def __init__(self, player_names: list) -> None:
+        self.n_player = len(player_names)
+        self.hint_token = 8
+        self.group_life_token = 3
+        self.card_pile = [
+            Card(color=color, value=k)
+                for color in CARDS_COLORS
+                    for k, v in CARDS_VALUES.items()
+                        for _ in range(v)]
+        random.shuffle(self.card_pile)
+        self.players = [Player(p, self.create_hand(self.n_player, self.card_pile)) for p in player_names]
 
-    def generate_game(self):
+        self.active_player = random.randrange(start=0, stop=len(player_names))
 
 
-    def distribute_cards():
-        pass
+    def create_hand(self, n_player: int, card_pile: list):
+        if n_player > 1 and n_player < 4:
+            n_card_to_assign = 4
+        elif n_player > 3 and n_player < 5:
+            n_card_to_assign = 5
+        else:
+            print("Error on number of player", file=sys.stderr)
+            return 
+
+        return [card_pile.pop() for _ in range(n_card_to_assign) ]
 
 
+if  __name__ == "__main__":
 
+    players_name = ["mario", "giovanni", "marco"]
+    gm = GameManager(players_name)
+
+    print()
